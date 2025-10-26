2cbcfcde13b1a92297dad872e4227faf25e3fe00
Didrik Wiig-Andersen <didrik@seas.upenn.edu>
2025-10-25 21:40:06 -0400

    bench_jxl.py and sample.ndpi

---
+++ scripts/bench_jxl.py
@@ -0,0 +1,23 @@
+
+
+# Settings
+SLIDE_PATH = "data/sample.ndpi"
+TILE_SIZE = 1024 # Adjust: 256, 1024, and 4096
+LEVEL = 0
+DISTANCE = 1.0
+EFFORT = 7
+SAVE_IMAGES = True
+
+
+
+def main():
+    print(f"Reading slide: {SLIDE_PATH}")
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
